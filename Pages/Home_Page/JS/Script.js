2bd3ece335302d0294ed1c44b140631accd185fa
سلطان <gbj6420@gmail.com>
2024-03-23 02:51:59 +0300

    update1

---
+++ Pages/Home_Page/JS/Script.js
@@ -0,0 +1,15 @@
+
+const type = new Typed("#title", {
+    strings: [
+    "أول ذكاء اصطناعي مصمم خصيصًا للإجابة على كل الأسئلة <span>الصحية</span>",
+    "أول ذكاء اصطناعي مصمم خصيصًا للإجابة على كل الأسئلة <span>اللياقية</span>",
+    "أول ذكاء اصطناعي مصمم خصيصًا للإجابة على كل الأسئلة <span>البدنية</span>",
+    "أول ذكاء اصطناعي مصمم خصيصًا للإجابة على كل الأسئلة <span>الطبيبية</span>",
+  ],
+  typeSpeed: 40,
+  smartBackspace: true,
+  startDelay: 5,
+  loop:true,
+  backSpeed: 20,
+  showCursor:false,
+});
